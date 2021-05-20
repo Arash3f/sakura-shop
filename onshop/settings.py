240be2e222f9b26bac03a85657f51636422ef806
Arash Alfoneh <arash.alfooneh@gmail.com>
2021-05-21 00:17:35 +0430

    try fic cors error

--- onshop/settings.py
+++ onshop/settings.py
@@ -71,6 +71,18 @@ MIDDLEWARE = [
 # cors
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
+default_headers = (
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+)
+
 CORS_ALLOW_METHODS = [
     'DELETE',
     'GET',
@@ -78,6 +90,17 @@ CORS_ALLOW_METHODS = [
     'PATCH',
     'POST',
     'PUT',
+]
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
 ]
 
 # CORS_ORIGIN_ALLOW_ALL = False
