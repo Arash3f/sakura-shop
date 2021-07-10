06d4073cd4a84f08ff1751e9d1f8a139a6628b44
Arash Alfoneh <arash.alfooneh@gmail.com>
2021-07-11 02:22:46 +0430

    change product url for host

--- product/urls.py
+++ product/urls.py
@@ -7,9 +7,10 @@ urlpatterns = [
     path('api/v1/product_list/', views.product_list.as_view() , name="product_list"),
     path('api/v1/top_product/' , views.top_product.as_view()  , name="top_product"),
     path('api/v1/best_product/', views.best_product.as_view() , name="best_product"),
-    re_path(r'api/v1/product/(?P<slug>[\w-]+)/', views.product_page.as_view()),
-    re_path(r'api/v1/similar_product/(?P<slug>[\w-]+)/', views.similar_product.as_view(), name="similar_product"),
-	re_path(r'api/v1/search/(?P<slug>[\w-]+)/' , views.product_search.as_view() , name = "search_product"),
+    
+    re_path(r'api/v1/product/(?P<slug>([^/]+))/', views.product_page.as_view()),
+    re_path(r'api/v1/similar_product/(?P<slug>([^/]+))/', views.similar_product.as_view(), name="similar_product"),
+	re_path(r'api/v1/search/(?P<slug>([^/]+))/' , views.product_search.as_view() , name = "search_product"),
     
     # pack :
     path('api/v1/pack_list/', views.pack_list.as_view()),
